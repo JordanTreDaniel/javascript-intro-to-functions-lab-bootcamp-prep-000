9002caec3c969bed59ac181a4a1068a98f2a22d3
christleyjordan@gmail.com Christley <christleyjordan@gmail.com>
2018-05-09 23:49:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,24 @@ function shout(string) {
 }
 
 function whisper(str) {
-  return string.toLowerCase();
+  return str.toLowerCase();
 }
 
-function 
+function logShout(str) {
+  console.log(str.toUpperCase());
+}
+
+function logWhisper(str) {
+  console.log(str.toLowerCase());
+}
+
+function sayHiToGrandma(str) {
+  if (str.toLowerCase() === str) {
+    return "I can't hear you!";
+  } else if (str.toUpperCase === str) {
+    return "YES INDEED!";
+  }
+  if (str === "I love you, Grandma.") {
+    return "I love you, too.";
+  }
+}
